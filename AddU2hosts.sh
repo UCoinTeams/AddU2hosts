9e21e542a5f32feb216bb4833e24e320e05873c6
Kevin.MX <aaawqw@live.cn>
2022-04-19 20:00:48 +0800

    AddU2hosts.sh: change working directory to .
    Fix: script fails to run if you cloned the whole repo / the script is inside some subfolders, not just /root.

--- AddU2hosts.sh
+++ AddU2hosts.sh
@@ -30,11 +30,9 @@ default_host() {
 # CloudflareST 测试程序
 cloudflare_st() {
   echo -e "${Green_font_prefix}> 正在下载 CloudflareST 测试程序${Font_color_suffix}";
-  wget -P /home/CloudflareSTtar ${web_proxy}https://cf.kevinmx.workers.dev/?url=https%3A%2F%2Fgithub.com%2FXIU2%2FCloudflareSpeedTest%2Freleases%2Fdownload%2Fv2.0.3%2FCloudflareST_linux_amd64.tar.gz -O CloudflareST_linux_amd64.tar.gz
-  tar -zxf /home/CloudflareSTtar/CloudflareST_linux_amd64.tar.gz -C /home/CloudflareSTtar
-  mv /home/CloudflareSTtar/CloudflareST /${USER}
-  mv /home/CloudflareSTtar/ip.txt /${USER}
-  chmod +x /${USER}/CloudflareST
+  wget ${web_proxy}https://cf.kevinmx.workers.dev/?url=https%3A%2F%2Fgithub.com%2FXIU2%2FCloudflareSpeedTest%2Freleases%2Fdownload%2Fv2.0.3%2FCloudflareST_linux_amd64.tar.gz -O CloudflareST_linux_amd64.tar.gz
+  tar -zxf CloudflareST_linux_amd64.tar.gz
+  chmod +x CloudflareST
   echo "104.25.26.31" > nowip.txt
 	echo -e "${Green_font_prefix}> 开始测速...${Font_color_suffix}";
 	NOWIP=$(head -1 nowip.txt)
@@ -52,10 +50,7 @@ cloudflare_st() {
 	sed -i 's/'${NOWIP}'/'${BESTIP}'/g' /etc/hosts
 	echo -e "${Green_font_prefix}> 完成...${Font_color_suffix}";
   echo -e "${Green_font_prefix}> 清理 CloudflareST 测试程序${Font_color_suffix}";
-  rm -rf /home/CloudflareSTtar*
-  rm -rf /${USER}/CloudflareST
-  rm -rf /${USER}/nowip.txt
-  rm -rf /${USER}/ip.txt
+  rm -rf *.txt cfst_hosts.sh result.csv CloudflareST*
 }
 
 
